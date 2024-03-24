59cf1a1c0a561a5ff8f1e50f02a87ef5663ee842
Christiaan Bloemendaal <github@tnrd.net>
2024-03-24 13:02:31 +0100

    feat: update for new metadata id and relation

--- Controllers/MetadataController.cs
+++ Controllers/MetadataController.cs
@@ -4,13 +4,13 @@ using TNRD.Zeepkist.WorkshopApi.Database.Models;
 
 namespace TNRD.Zeepkist.WorkshopApi.Backend.JsonApi.Controllers;
 
-public class MetadataController : ReadOnlyController<Metadata, string>
+public class MetadataController : ReadOnlyController<Metadata, int>
 {
     public MetadataController(
         IJsonApiOptions options,
         IResourceGraph resourceGraph,
         ILoggerFactory loggerFactory,
-        IResourceQueryService<Metadata, string> resourceService
+        IResourceQueryService<Metadata, int> resourceService
     )
         : base(options, resourceGraph, loggerFactory, resourceService)
     {
